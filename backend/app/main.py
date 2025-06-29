ddb35709c321c526a3920eb49acfaadf32e912ee
Ismael Jose <jose17isma@gmail.com>
2025-06-28 22:29:42 -0500

    dependencies 6

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,16 +1,13 @@
 from fastapi import FastAPI
-from app.db import db
+from app.db.db import init_db
 from app.api.endpoints import cliente, factura
-
-# Crear tablas si no existen
-db.Base.metadata.create_all(bind=db.engine)
 
 # Instancia principal de la aplicación
 app = FastAPI(
     title="API Clientes y Facturación",
     version="1.0.0"
 )
-
+init_db()
 # Incluir routers
 app.include_router(cliente.router, prefix="/api/clientes", tags=["Clientes"])
 app.include_router(factura.router, prefix="/api/facturas", tags=["Facturas"])
