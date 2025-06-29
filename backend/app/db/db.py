ecf60bff1ccbc863a75c9934f3a1a52435f0387d
Ismael Jose <jose17isma@gmail.com>
2025-06-28 22:26:04 -0500

    dependencies 4

--- backend/app/db/db.py
+++ backend/app/db/db.py
@@ -1,8 +1,7 @@
 from app.db.session import engine
-from app.models import user, cliente, factura, factura_detalle
+from app.models import cliente, factura
 
 def init_db():
-    user.Base.metadata.create_all(bind=engine)
     cliente.Base.metadata.create_all(bind=engine)
     factura.Base.metadata.create_all(bind=engine)
-    factura_detalle.Base.metadata.create_all(bind=engine)
+
