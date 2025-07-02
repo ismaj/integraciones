55154eb58413d603cb9984ab459b08e00f96edf6
Ismael Jose <jose17isma@gmail.com>
2025-07-01 23:10:43 -0500

    front 1

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,12 +1,24 @@
 from fastapi import FastAPI
 from app.db.db import init_db
 from app.api.endpoints import cliente, factura,auth
+from fastapi.middleware.cors import CORSMiddleware
 
 # Instancia principal de la aplicación
 app = FastAPI(
     title="API Clientes y Facturación",
     version="1.0.0"
 )
+from fastapi.middleware.cors import CORSMiddleware
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://192.168.1.5:5173"],  # Cambia al origen real del frontend
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+
 init_db()
 # Incluir routers
 app.include_router(cliente.router, prefix="/api/clientes", tags=["Clientes"])
